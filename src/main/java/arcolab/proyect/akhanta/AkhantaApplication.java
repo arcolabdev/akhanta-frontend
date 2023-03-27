0f7d6c3d14c735b5db0deaffdfc91b35bf16b144
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2023-03-27 14:44:35 -0300

    primer commmit

---
+++ src/main/java/arcolab/proyect/akhanta/AkhantaApplication.java
@@ -0,0 +1,13 @@
+package arcolab.proyect.akhanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkhantaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkhantaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/arcolab/proyect/akhanta/AkhantaApplicationTests.java
@@ -0,0 +1,13 @@
+package arcolab.proyect.akhanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkhantaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
