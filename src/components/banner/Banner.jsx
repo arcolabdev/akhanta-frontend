8d5038a9579d204229e9fa8762fdfa3e8caa28f7
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2023-04-11 01:58:07 -0300

    first banner edit

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import './Banner.css'
+import React from "react";
+import "./Banner.css";
 
 const Banner = () => {
   return (
-    <section className='banner-content container'>
-      <div className='banner-flex'>
-        <h1 className='banner-title'>
-          "Para la mente que está quieta,<br/>
-          <span> todo el universo se rinde</span>"
-        </h1>
-        <p className='banner-author'>-Lao Tse</p>
+    <section className="banner-content container">
+      <div className="banner-flex">
+        <span className="banner-title">
+          <span className="banner-title-quotes">"</span>
+          <span>Para la mente que está quieta,</span>
+          <br />
+          <span className="banner-title-phrase-2">
+            todo el universo se rinde
+          </span>
+          <span className="banner-title-quotes">"</span>
+        </span>
+        <p className="banner-author">-Lao Tse</p>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
