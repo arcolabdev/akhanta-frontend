9b4f1695632c669c702735f086f79df7496e7657
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2023-04-10 21:51:56 -0300

    primeros estilos de formulario de contacto

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,49 +1,55 @@
-import React from 'react'
-import './Footer.css'
+import React, { useState } from "react";
+import "./Footer.css";
 
 const Footer = () => {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const phoneNumber = '573142716508'; // Número de teléfono al que se enviará el mensaje
+    const whatsappMessage = `Hola, soy ${name} y quiero enviar el siguiente mensaje: ${message}`;
+    const fullUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(whatsappMessage)}`; 
+    window.open(fullUrl, "_blank");
+  };
+
   return (
-    <footer className='footer-section' id='contact'>
-      <div className='footer-content container'>
-        <h1 className='footer-title'>Contáctanos</h1>
-        <div className='footer-box'>
-        <form className='footer-form' >
-          <div className='footer-form-div' >
-            <label className='footer-form-tag'>Name</label>
-            <input
-              type='text'
-              name='name'
-              className='footer-form-input'
-              placeholder='Inserta tu nombre'
-            ></input>
-          </div>
-          <div className='footer-form-div'>
-            <label className='footer-form-tag' >Email</label>
-            <input
-              type='email'
-              name='email'
-              className='footer-form-input'
-              placeholder='Inserta tu email'
-
-            ></input>
-          </div>
-          <div className='footer-form-div contact-form-area'>
-            <label className='footer-form-tag' >Mensaje</label>
-            <textarea name='message' cols='30' rows='10' className='contact-form-input' placeholder='Escribí tu mensaje' ></textarea>
-          </div>
-
-          <button className='contact-button'>
-            Enviar mensaje
-          </button>
-        </form>
-        <ul className='footer-nav-media'>
-          <li>Facebook</li>
-          <li>Instagram</li>
-        </ul>
+    <footer className="footer-section" id="contact">
+      <div className="footer-content container">
+        <h1 className="footer-title">Contáctanos</h1>
+        <div className="footer-box">
+          <form className="footer-form" onSubmit={handleSubmit}>
+            <div className="footer-form-div">
+              <input
+                type="text"
+                name="name"
+                className="footer-form-input"
+                placeholder="Nombre"
+                onChange={(e) => setName(e.target.value)}
+              ></input>
+            </div>
+            <div className="footer-form-div contact-form-area">
+              <textarea
+                name="message"
+                cols="30"
+                rows="10"
+                className="contact-form-input"
+                placeholder="Dejanos tu mensaje..."
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
+            </div>
+            <button type="submit" className="contact-button">
+              Enviar mensaje
+            </button>
+          </form>
+          <ul className="footer-nav-media">
+            <li>Facebook</li>
+            <li>Instagram</li>
+          </ul>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
