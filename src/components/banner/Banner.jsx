93809c5359dcdc0374525dc7c94462c57329b42e
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2023-04-12 19:43:51 -0300

    arreglado el banner

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,15 +5,17 @@ const Banner = () => {
   return (
     <section className="banner-content container">
       <div className="banner-flex">
-        <span className="banner-title">
-          <span className="banner-title-quotes">"</span>
-          <span>Para la mente que está quieta,</span>
-          <br />
-          <span className="banner-title-phrase-2">
-            todo el universo se rinde
-          </span>
-          <span className="banner-title-quotes">"</span>
-        </span>
+        <div className="banner-title">
+          <div className="banner-title-quotes">"</div>
+          <div className="banner-title-text">
+            <span>Para la mente que está quieta,</span>
+            <br />
+            <span className="banner-title-phrase-2">
+              todo el universo se rinde
+            </span>
+            <span className="banner-title-quotes">"</span>
+          </div>
+        </div>
         <p className="banner-author">-Lao Tse</p>
       </div>
     </section>
