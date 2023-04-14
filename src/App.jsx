f172f53b1bad64761cbb12405c0c8a750902f89a
VilattaRomina <vilattaromina@gmail.com>
2023-04-14 16:11:40 -0300

    agrego enrutamiento

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Login from './pages/Login/Login';
+import AssociatedDetail from './pages/associatedDetail/AssociatedDetail';
+import Home from './pages/home/Home';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/associated/:id" element={<AssociatedDetail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
