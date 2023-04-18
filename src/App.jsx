fcb56e09708120abdf0eeb9859e9c6939849a237
IsabelaAcostaB <anacosta21@icloud.com>
2023-04-18 01:23:52 -0500

    Responsive page Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import Login from "./pages/Login/Login";
 import AssociatedDetail from "./pages/associatedDetail/AssociatedDetail";
-import Home from "./pages/Home/Home";
+import Home from "./pages/home/Home";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Formulario from "./pages/DBTest/Form";
+// import Formulario from "./pages/DBTest/Form";
 
 function App() {
   return (
