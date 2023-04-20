8bedfbc8b2dd7effbf5c228e16d1e57df28aa9ac
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2023-04-20 19:20:32 -0300

    changed associated url to production

--- src/pages/associatedDetail/AssociatedDetail.jsx
+++ src/pages/associatedDetail/AssociatedDetail.jsx
@@ -14,7 +14,7 @@ const AssociatedDetail = () => {
 
     useEffect(() => {
         axios
-            .get(`http://localhost:8080/api/v1/associates/${id}`)
+            .get(`http://akhanta.herokuapp.com/api/v1/associates/${id}`)
             .then((res) => {
                 setAssociated(res.data);
             })
