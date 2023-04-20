7148b4dd7688c4527b46df154af16f2c8d3b2b80
VilattaRomina <vilattaromina@gmail.com>
2023-04-20 13:30:30 -0300

    integracion de front con back associated

--- src/pages/associatedDetail/AssociatedDetail.jsx
+++ src/pages/associatedDetail/AssociatedDetail.jsx
@@ -1,35 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../components/header/Header'
-import imagenDetail from '../../assets/imagendetail.png'
 import './AssociatedDetail.css'
-import { FaInstagram, FaTwitter, FaFacebookF} from 'react-icons/fa'
-import { MdArrowBackIosNew} from 'react-icons/md'
-import { Link } from 'react-router-dom';
+import { FaInstagram, FaFacebookF, } from 'react-icons/fa'
+import { MdArrowBackIosNew, MdWhatsapp } from 'react-icons/md'
+import { Link, useParams } from 'react-router-dom';
+import axios from "axios"
+
 
 const AssociatedDetail = () => {
+
+    const [associated, setAssociated] = useState(null)
+    const { id } = useParams();
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:8080/api/v1/associates/${id}`)
+            .then((res) => {
+                setAssociated(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [id]);
+
+    if (!associated) {
+        return null; 
+      }
+
+    const { banner, description, name, links } = associated.results || {};
+
     return (
         <>
             <section className='background-banner'>
                 <Header />
-                <h1>Gimnasio Yoga</h1>
+                <h1>{name}</h1>
             </section>
             <section className='about-section'>
                 <div className='about-contain'>
                     <div className='info-contain'>
-                        <h3>Hola,<br /> <span>Soy Jane</span></h3>
-                        <p>Namaste y bienvenidos a todos. Soy una maestra de yoga y estoy aquí para guiarlos en un viaje de autoexploración y autodescubrimiento a través de la práctica del yoga. La práctica del yoga no se trata solo de estirar el cuerpo y hacer posturas complicadas. Se trata de encontrar la conexión entre nuestra mente, cuerpo y espíritu, y descubrir cómo podemos vivir una vida más plena y consciente. Acompáñenme en este camino de autoconocimiento y crecimiento personal, mientras exploramos juntos el maravilloso mundo del yoga.</p>
+                        <h3>Hola,<br /> <span>Soy {name}</span></h3>
+                        <p>{description}</p>
 
                     </div>
                     <div className='image-contain'>
-                    <img src={imagenDetail} alt='imagen-asociado' height={500} />
+                        <img src={banner} alt='imagen-asociado' height={500} />
                     </div>
                 </div>
-                <Link to='/'><div className='arrow'><MdArrowBackIosNew size={35} color='#C8ABFB'/></div></Link>
+                <Link to='/'><div className='arrow'><MdArrowBackIosNew size={35} color='#C8ABFB' /></div></Link>
             </section>
             <footer className='section-footer'>
-                <a href="." target="_blank"><FaInstagram  size={35} color='#C3BEAA'/></a>
-                <a href="." target="_blank"><FaTwitter size={35} color='#C3BEAA'/></a>
-                <a href="." target="_blank"><FaFacebookF size={35} color='#C3BEAA'/></a>
+                {links && links.map(link => {
+                    switch (link.tag) {
+                        case "INSTAGRAM":
+                            return <a key={link.id} href={link.url} target="_blank" rel="noreferrer"><FaInstagram size={35} color='#C3BEAA' /></a>
+                        case "FACEBOOK":
+                            return <a key={link.id} href={link.url} target="_blank" rel="noreferrer"><FaFacebookF size={35} color='#C3BEAA' /></a>
+                        case "WHATSAPP":
+                            return <a key={link.id} href={link.url} target="_blank" rel="noreferrer"><MdWhatsapp size={35} color='#C3BEAA' /></a>
+                        default:
+                            return null; // no renderizar nada si el tag no coincide
+                    }
+                })}
             </footer>
         </>
     )
