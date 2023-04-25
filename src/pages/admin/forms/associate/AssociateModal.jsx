0eeda1e2a5c9874ec4681b9588c6217caad78a7a
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2023-04-25 03:37:28 -0300

    modal merge

--- src/pages/admin/forms/associate/AssociateModal.jsx
+++ src/pages/admin/forms/associate/AssociateModal.jsx
@@ -1,76 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
-<<<<<<< HEAD
-import Form from "react-bootstrap/Form";
-import Modal from "react-bootstrap/Modal";
-import { FaEdit, FaTrash } from "react-icons/fa/";
-
-function AssociateModal({ associateId, associateName }) {
-  const [showEdit, setShowEdit] = useState(false);
-  const handleCloseEdit = () => setShowEdit(false);
-  const handleshowEdit = () => setShowEdit(true);
-
-  const [showDelete, setShowDelete] = useState(false);
-  const handleCloseDelete = () => setShowDelete(false);
-  const handleshowDelete = () => setShowDelete(true);
-
-  const handleDelete = (id) => {
-    axios
-      .delete("http://akhanta.herokuapp.com/api/v1/associates/" + id)
-      .then((data) => console.log(data));
-    setShowDelete(false);
-  };
-
-  return (
-    <>
-      <Button variant="info" onClick={handleshowEdit}>
-        <FaEdit />
-      </Button>
-
-      <Button variant="danger" onClick={handleshowDelete}>
-        <FaTrash />
-      </Button>
-      <Modal show={showEdit} onHide={handleshowDelete}>
-        <Modal.Header closeButton>
-          <Modal.Title>Editar Asociado</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="name@example.com"
-                autoFocus
-              />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Example textarea</Form.Label>
-              <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseEdit}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleDelete}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      <Modal show={showDelete} onHide={handleCloseDelete}>
-        <Modal.Header closeButton>
-          <Modal.Title>Eliminar Asociado</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Seguro que deseas eliminar {associateName}?</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseDelete}>
-=======
 import Modal from "react-bootstrap/Modal";
 import { FaTrash } from "react-icons/fa/";
 
@@ -99,7 +29,6 @@ function AssociateModal({ associateId, associateName }) {
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={() => setShow(false)}>
->>>>>>> develop
             Cancelar
           </Button>
           <Button variant="danger" onClick={() => handleDelete(associateId)}>
@@ -107,11 +36,7 @@ function AssociateModal({ associateId, associateName }) {
           </Button>
         </Modal.Footer>
       </Modal>
-<<<<<<< HEAD
-    </>
-=======
     </div>
->>>>>>> develop
   );
 }
 
