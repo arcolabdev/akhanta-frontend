fd5e98417905e0b641cb60f87dc9e5206af8c00a
zoezn <zxejimenez@gmail.com>
2023-07-26 01:57:21 -0300

    elimine comentarios y comente logica NO funcional de paginacion

--- src/pages/articles/ArticlesPage.jsx
+++ src/pages/articles/ArticlesPage.jsx
@@ -1,4 +1,5 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useEffect } from "react";
+// import React, { useContext, useState, useEffect } from "react";
 import "./ArticlesPage.css";
 import AltHeader from "../../components/alt-header/AltHeader";
 import Article from "../../components/article/Article";
@@ -6,34 +7,34 @@ import { articlesResponse } from "../../utils/Utils";
 import ScrollToTopButton from "../../components/autoscroll/ScrollToTopButton";
 
 const ArticlesPage = () => {
-  const [page, setPage] = useState(1);
-  const [pageMax, setPageMax] = useState(null);
-  const [pages, setPages] = useState(null);
-  const [indexStart, setIndexStart] = useState(0);
-  const [indexEnd, setIndexEnd] = useState(4);
-  const [articlesArray, setArticlesArray] = useState(null);
+  // const [page, setPage] = useState(1);
+  // const [pageMax, setPageMax] = useState(null);
+  // const [pages, setPages] = useState(null);
+  // const [indexStart, setIndexStart] = useState(0);
+  // const [indexEnd, setIndexEnd] = useState(4);
+  // const [articlesArray, setArticlesArray] = useState(null);
   const lastIndex = articlesResponse.length - 1;
 
-  let pagesArray = [];
+  // let pagesArray = [];
 
-  useEffect(() => {
-    setPageMax(Math.round(articlesResponse.length / 4));
-  }, [articlesResponse]);
+  // useEffect(() => {
+  //   setPageMax(Math.round(articlesResponse.length / 4));
+  // }, [articlesResponse]);
 
-  useEffect(() => {
-    setIndexStart(page * 4 - 1);
-    setIndexEnd(indexStart + 4);
-  }, [page, indexStart]);
+  // useEffect(() => {
+  //   setIndexStart(page * 4 - 1);
+  //   setIndexEnd(indexStart + 4);
+  // }, [page, indexStart]);
 
-  useEffect(() => {
-    setArticlesArray(articlesResponse.slice(indexStart, indexEnd));
-  }, [indexStart, indexEnd]);
+  // useEffect(() => {
+  //   setArticlesArray(articlesResponse.slice(indexStart, indexEnd));
+  // }, [indexStart, indexEnd]);
 
-  useEffect(() => {
-    for (let i = 1; i <= pageMax; i++) {
-      pagesArray.push(i);
-    }
-  }, [pageMax]);
+  // useEffect(() => {
+  //   for (let i = 1; i <= pageMax; i++) {
+  //     pagesArray.push(i);
+  //   }
+  // }, [pageMax]);
 
   useEffect(
     () =>
