0078eda67128e641e5824973056c4dc5ba27b5d7
VilattaRomina <vilattaromina@gmail.com>
2023-10-22 12:23:37 -0300

    cambios en calendario

--- src/components/schedule/Schedule.jsx
+++ src/components/schedule/Schedule.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 import Grid from "./grid/Grid";
-import './Schedule.css';
+import "./Schedule.css";
+import mandala_left from "../../assets/mandalas-left.png";
+import mandala_right from "../../assets/mandalas-right.png";
+import mandala_mobile_left from "../../assets/mandalas-mobile-left.png";
+import mandala_mobile_right from "../../assets/mandalas-mobile-right.png";
 
 const Schedule = () => {
   return (
     <section className="schedule-section " id="horarios">
+      <div className="mandala-desktop">
+        <img
+          src={mandala_left}
+          alt="decoration"
+          className="mandala-image mandala-desktop"
+        />
+      </div>
+      <div className="mandala-mobile">
+        <img
+          src={mandala_mobile_left}
+          alt="decoration"
+          className="mandala-image"
+        />
+      </div>
       <div className="schedule-content ">
-        <h2>Horarios</h2>
         <Grid />
+      </div>
+      <div className="mandala-desktop">
+        <img src={mandala_right} alt="decoration" className="mandala-image" />
+      </div>
+      <div className="mandala-mobile">
+        <img
+          src={mandala_mobile_right}
+          alt="decoration"
+          className="mandala-image"
+        />
       </div>
     </section>
   );
